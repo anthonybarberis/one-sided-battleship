4f4a937c5b3d9ac7f16eb00e712e33d4a4d123ca
Anthony Barberis <skamunism@gmail.com>
2020-05-02 13:52:11 -0700

    Changed vars to lets

--- scripts.js
+++ scripts.js
@@ -3,12 +3,12 @@ function init() {
 }
 
 function createGrid() {
-    var board = document.getElementById("board");
+    let board = document.getElementById("board");
 
     for (squares=0; squares < 100; squares++) {
         
         //create square node
-        var square = document.createElement("div");
+        let square = document.createElement("div");
         square.className += "square";
         square.textContent = "A1"
         
