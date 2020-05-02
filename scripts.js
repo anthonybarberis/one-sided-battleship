233e684dc2cdb2ed903440e6d31df6954c2bb188
Anthony Barberis <skamunism@gmail.com>
2020-05-02 12:45:17 -0700

    Removed redundant gridLabel

--- scripts.js
+++ scripts.js
@@ -6,8 +6,17 @@ function createGrid() {
     var board = document.getElementById("board");
 
     for (squares=0; squares < 100; squares++) {
-        var square = document.createElement("div")
+        //create grid label node
+        var gridLabel = document.createElement("span");
+        gridLabel.className += "gridLabel";
+        gridLabel.textContent = "A1";
+        
+        //create square node
+        var square = document.createElement("div");
         square.className += "square";
+        square.textContent = "A1"
+        
+        //create squares
         board.appendChild(square);
     }
 }
