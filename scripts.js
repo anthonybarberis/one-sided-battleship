459b6d8f43e8bda2d789c325232e83b28dc6bf2b
Anthony Barberis <skamunism@gmail.com>
2020-05-02 14:18:05 -0700

    Redid grid loop to label and id each square

--- scripts.js
+++ scripts.js
@@ -5,15 +5,20 @@ function init() {
 function createGrid() {
     let board = document.getElementById("board");
 
-    for (squares=0; squares < 100; squares++) {
-        
-        //create square node
-        let square = document.createElement("div");
-        square.className += "square";
-        square.textContent = "A1"
-        
-        //create squares
-        board.appendChild(square);
+    let rowLabels = "ABCDEFGHIJ"
+
+    for (columns = 0; columns < 10; columns++) {
+        let row = rowLabels.slice(columns, columns + 1)
+        for (rows = 0; rows < 10; rows++) {
+
+            //create square node
+            let square = document.createElement("div");
+            square.className += "square";
+            square.textContent = square.id = row + (rows+1);
+
+            //create squares
+            board.appendChild(square);
+        }
     }
 }
 
