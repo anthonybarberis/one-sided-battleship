d4f45616b14451d60ead5bc19de22cd431f55f1b
Anthony Barberis <skamunism@gmail.com>
2020-05-02 11:45:06 -0700

    Grid for loop

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 function init() {
-    console.log("Hello World")
+    createGrid();
+}
+
+function createGrid() {
+    var board = document.getElementById("board");
+
+    //create the grid square template
+    var square = document.createElement("div")
+
+    //create the grid squares
+    for (squares=0; squares < 100; squares++) {
+        console.log(squares+1)
+    }
 }
 
 window.onload = init;
